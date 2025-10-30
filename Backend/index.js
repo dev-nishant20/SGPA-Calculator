c2b5a4a72962a6fb25d388c2b8e90ef2f358e12d
Nishant Prajapati <blognishant55@gmail.com>
2025-10-30 21:29:10 +0530

    Change port to use environment variable or default

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-let port = 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
